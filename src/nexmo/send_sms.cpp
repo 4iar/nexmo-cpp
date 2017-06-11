43abca9c6855067e3bd0ddc2e06e3557e7a58712
4iar <me@4iar.com>
2017-06-11 15:46:26 +0100

    Fix to and from variables

--- src/nexmo/send_sms.cpp
+++ src/nexmo/send_sms.cpp
@@ -11,12 +11,11 @@ void Nexmo::sendSms(std::string sender, std::string recipient, std::string messa
     json body;
     body["api_key"] = this->key;
     body["api_secret"] = this->secret;
-    body["to"] = sender;
-    body["from"] = recipient;
+    body["to"] = recipient;
+    body["from"] = sender;
     body["text"] = message;
 
     std::string url = "https://rest.nexmo.com/sms/json";
-
 
     this->client->post(url, body.dump());
 };
