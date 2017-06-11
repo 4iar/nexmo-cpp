ee3871befa60f4814ca3a0f405a024f26e7229fb
4iar <me@4iar.com>
2017-06-11 22:30:30 +0100

    Refactor tests to use fixtures

--- test/nexmo/send_sms.test.cpp
+++ test/nexmo/send_sms.test.cpp
@@ -11,50 +11,43 @@ using ::testing::_;
 using json = nlohmann::json;
 
 
-TEST(sendSmsTest, callsClientAppropriately) {
+// Test fixtures
+class SendSmsTest : public ::testing::Test {
+public:
+    MockClient client;
+    Nexmo nexmo;
+
+    // API credentials
     std::string key = "API_KEY";
     std::string secret = "API_SECRET";
 
-    MockClient client;
-    Nexmo nexmo(key, secret, &client);
-
+    // Message details
     std::string sender = "441632960960";
     std::string recipient = "441632960960";
     std::string message = "Hello from Nexmo";
 
-    const std::string expected_url = "https://rest.nexmo.com/sms/json";
+    std::string expected_url = "https://rest.nexmo.com/sms/json";
     json expected_body;
-    expected_body["api_key"] = key;
-    expected_body["api_secret"] = secret;
-    expected_body["to"] = recipient;
-    expected_body["from"] = sender;
-    expected_body["text"] = message;
 
+    void SetUp() {
+        nexmo = Nexmo(key, secret, &client);
+
+        expected_body["api_key"] = key;
+        expected_body["api_secret"] = secret;
+        expected_body["to"] = recipient;
+        expected_body["from"] = sender;
+        expected_body["text"] = message;
+    };
+};
+
+TEST_F(SendSmsTest, callsClientAppropriately) {
     EXPECT_CALL(client, post(expected_url, expected_body.dump()))
         .Times(AtLeast(1));
 
     nexmo.sendSms(sender, recipient, message);
 }
 
-TEST(sendSmsTest, handlesResponseData) {
-    std::string key = "API_KEY";
-    std::string secret = "API_SECRET";
-
-    MockClient client;
-    Nexmo nexmo(key, secret, &client);
-
-    std::string sender = "441632960960";
-    std::string recipient = "441632960960";
-    std::string message = "Hello from Nexmo";
-
-    const std::string expected_url = "https://rest.nexmo.com/sms/json";
-    json expected_body;
-    expected_body["api_key"] = key;
-    expected_body["api_secret"] = secret;
-    expected_body["to"] = recipient;
-    expected_body["from"] = sender;
-    expected_body["text"] = message;
-
+TEST_F(SendSmsTest, handlesResponseData) {
     std::string fake_response = "{messages: [{fakeresponsedata}]}";
 
     EXPECT_CALL(client, post(expected_url, expected_body.dump()))
